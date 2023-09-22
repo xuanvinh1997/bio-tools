974f7f5381767521a639c888df93e9b6ec7aff19
Vinh Pham <vinhpx@MacBook-Pro-cua-macbook.local>
2023-09-22 15:13:34 +0700

    init

---
+++ install_amber.sh
@@ -0,0 +1,11 @@
+!# /bin/sh
+apt update
+apt upgrade
+apt install cmake
+apt -y update
+apt -y install tcsh make
+apt install gcc
+apt -y install gcc gfortran
+apt -y install flex bison patch
+apt -y install bc xorg-dev libbz2-dev wget
+apt-get install gcc build-essential
